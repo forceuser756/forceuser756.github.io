cfd7071f45604fd085e903b525dd347231c5dd07
forceuser756 <42787700+forceuser756@users.noreply.github.com>
2020-04-17 20:34:48 -0400

    Update

--- pages/api/pokemon/typeList/[typeList].js
+++ pages/api/pokemon/typeList/[typeList].js
@@ -1,14 +1,24 @@
-import getPokemon from 'json-pokemon/getPokemon';
+import pokemon from 'json-pokemon';
 
 export default (req, res) => {
 
-    let result = {"error": "Could not find a Pokemon with this Type."};
+    let result = [];
 
-    let pokemon = getPokemon.getPokemonByName(req.query.type);
+    const type = req.query.type;
 
-    if(pokemon !== null) {
-        result = pokemon;
-    }
+    pokemon.forEach((pokemonObject) => {
+
+      pokemonObject.typeList.forEach((typeProperty) => {
+
+        if(type === typeProperty) {
+
+          result.push(pokemonObject);
+
+        }
+
+      });
+
+    });
 
     res.json(result);
 
